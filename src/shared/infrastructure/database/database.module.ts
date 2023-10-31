1a09af8753acfcdae0431b1a91f261f75103674e
wilfoz <wil_foz@yahoo.com.br>
2023-10-31 15:16:28 -0300

    feat: :sparkles:

---
+++ src/shared/infrastructure/database/database.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
+import { EnvConfigModule } from '../env-config/env-config.module';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [EnvConfigModule.forRoot()],
+  providers: [ConfigService, PrismaService],
+})
+export class DatabaseModule { }
